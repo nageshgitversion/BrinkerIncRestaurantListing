39458c0ca86cecef31acba78e7bb52aab9107bb6
nageshgitversion <rasukuntanagesh@gmail.com>
2024-08-20 11:10:59 +0530

    db url

--- src/main/java/com/BrinkerInc/BrinkerIncRestaurantListing/BrinkerIncRestaurantListingApplication.java
+++ src/main/java/com/BrinkerInc/BrinkerIncRestaurantListing/BrinkerIncRestaurantListingApplication.java
@@ -3,13 +3,20 @@ package com.BrinkerInc.BrinkerIncRestaurantListing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class BrinkerIncRestaurantListingApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrinkerIncRestaurantListingApplication.class, args);
+		
+		
 	}
 
+	
+	
 }
